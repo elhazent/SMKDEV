ea8455233b6a391bf06afbf9fb37bbd890a0decb
averoes12 <hz.ir.daff@gmail.com>
2020-11-04 18:05:20 +0700

    add json data

---
+++ lib/feature/main/home/home-presenter.dart
@@ -0,0 +1,13 @@
+import 'package:smkdevapp/base/base-presenter.dart';
+
+abstract class HomeContract extends BaseContract{
+
+}
+
+class HomePresenter extends BasePresenter<HomeContract>{
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+}
---
+++ lib/feature/main/main-presenter.dart
@@ -0,0 +1,13 @@
+import 'package:smkdevapp/base/base-presenter.dart';
+
+abstract class MainContract extends BaseContract{
+
+}
+
+class MainPresenter extends BasePresenter<MainContract>{
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+}
