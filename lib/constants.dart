90db0dcadba3c98f7ce72c9c6c89ca4c2434ec36
Elhazent <elhazent.mail@gmail.com>
2020-11-04 15:59:42 +0700

    initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class DefaultKey{
+  static const AccessToken = "TOKEN";
+
+
+}
+
+class DefaultModul{
+  static const Auth = "auth";
+}
+
+class DefaultColor{
+  static final buttonPrimary = Colors.grey.withOpacity(0.4);
+  static final primaryColor = Colors.blue;
+  static final secondaryColor = Color(0xffC1B265);
+  static final background = Colors.grey.shade200;
+  static final textField = Color(0xFFDDDDDD).withOpacity(0.5);
+
+}
+
+class DefaultDimen {
+  static double spaceDoubleExtraLarge = 64.0;
+  static double spaceExtraLarge = 32.0;
+  static const double spaceMidLarge = 24.0;
+  static double spaceLarge = 16.0;
+  static double spaceMedium = 12.0;
+  static double spaceSmall = 8.0;
+  static double spaceTiny = 4.0;
+
+  static double textDoubleExtraLarge = 48.0;
+  static double textExtraLarge = 20.0;
+  static double textLarge = 18.0;
+  static double textMedium = 14.0;
+  static double textRegular = 12.0;
+  static double textSmall = 11.0;
+  static double textTiny = 8.0;
+
+  static double buttonTiny = 24.0;
+  static double buttonSmall = 32.0;
+  static double buttonRegular = 48.0;
+  static double buttonMedium = 56.0;
+
+  static double radius = 4.0;
+  static double ratioThumbnail = 16 / 9;
+  static double ratioBanner = 5 / 2;
+  static double ratioSquare = 1;
+
+  static double iconTiny = 12;
+  static double iconSmall = 16;
+  static double iconRegular = 24;
+  static double iconLarge = 32;
+
+  static double avatarLarge = 96;
+  static double avatarRegular = 48;
+  static double avatarSmall = 32;
+  static double avatarTiny = 24;
+
+}
+
+class DefaultFontWeight {
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+}
+
+class DefaultImageLocation {
+  static const String _baseImageLocation = 'assets/images';
+  static const String iconHome = 'assets/images/icon_home.svg';
+  static const String iconCalendar = 'assets/images/icon_calendar.svg';
+  static const String iconLayanan = 'assets/images/icon_layanan.svg';
+  static const String iconMore = 'assets/images/icon_more.svg';
+  static const String iconUser = 'assets/images/icon_user.svg';
+  static const String contoh = 'assets/images/contoh.jpg';
+
+}
---
+++ lib/feature/main/main-page.dart
@@ -0,0 +1,273 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:smkdevapp/base/base-state.dart';
+import 'package:smkdevapp/feature/main/main-presenter.dart';
+import 'home/home-page.dart';
+import '../../constants.dart';
+
+class MainPage extends BaseStatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends BaseState<MainPage,MainPresenter> implements MainContract {
+  int currentIndex = 0;
+  List<NavigationIconView> _navigationViews;
+
+  @override
+  void initMvp() {
+    super.initMvp();
+    presenter = MainPresenter();
+    presenter.setView(this);
+
+  }
+
+
+  @override
+  Widget buildWidgetUI(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return new Center(
+          child: _navigationViews[currentIndex].child,
+        );
+      },
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    _navigationViews = <NavigationIconView>[
+      NavigationIconView(
+        child: HomePage(),
+        icon: buildIconAsset(asset: DefaultImageLocation.iconHome),
+        activeIcon: SvgPicture.asset(
+          DefaultImageLocation.iconHome,
+          width: 16,
+          height: 16,
+          color: DefaultColor.primaryColor,
+        ),
+        title: Text(
+          "Home",
+          style: TextStyle(
+              color: currentIndex == 0
+                  ? DefaultColor.primaryColor
+                  : Colors.black),
+        ),
+      ),
+      NavigationIconView(
+        child: Container(
+          child: Center(
+            child: Text(
+              "LAYANAN"
+            ),
+          ),
+        ),
+        icon: buildIconAsset(asset: DefaultImageLocation.iconLayanan),
+        activeIcon: SvgPicture.asset(
+          DefaultImageLocation.iconLayanan,
+          width: 16,
+          height: 16,
+          color: DefaultColor.primaryColor,
+        ),
+        title: Text(
+          "Layanan",
+          style: TextStyle(
+              color: currentIndex == 1
+                  ? DefaultColor.primaryColor
+                  : Colors.black),
+        ),
+      ),
+      NavigationIconView(
+        child: Container(
+          child: Center(
+            child: Text(
+              "BOOKING"
+            ),
+          ),
+        ),
+        icon: buildIconAsset(asset: DefaultImageLocation.iconCalendar),
+        activeIcon: SvgPicture.asset(
+          DefaultImageLocation.iconCalendar,
+          width: 16,
+          height: 16,
+          color: DefaultColor.primaryColor,
+        ),
+        title: Text(
+          "Booking",
+          style: TextStyle(
+              color: currentIndex == 2
+                  ? DefaultColor.primaryColor
+                  : Colors.black),
+        ),
+      ),
+      NavigationIconView(
+        child: Container(
+          child: Center(
+            child: Text(
+              "PROFILE"
+            ),
+          ),
+        ),
+        icon: Stack(
+          children: <Widget>[
+            new Icon(Icons.person_outline),
+            new Positioned(
+              right: 0,
+              child: new Container(
+                padding: EdgeInsets.all(1),
+                decoration: new BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                constraints: BoxConstraints(
+                  minWidth: 12,
+                  minHeight: 12,
+                ),
+                child: new Text(
+                  '2',
+                  style: new TextStyle(
+                    color: Colors.white,
+                    fontSize: 8,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            )
+          ],
+        ),
+        activeIcon: SvgPicture.asset(
+          DefaultImageLocation.iconUser,
+          width: 16,
+          height: 16,
+          color: DefaultColor.primaryColor,
+        ),
+        title: Text(
+          "Profile",
+          style: TextStyle(
+              color: currentIndex == 3
+                  ? DefaultColor.primaryColor
+                  : Colors.black),
+        ),
+      ),
+      NavigationIconView(
+        child: Container(
+          child: Center(
+            child: Text(
+              "MORE"
+            ),
+          ),
+        ),
+        icon: buildIconAsset(asset: DefaultImageLocation.iconMore),
+        activeIcon: SvgPicture.asset(
+          DefaultImageLocation.iconMore,
+          width: 16,
+          height: 16,
+          color: DefaultColor.primaryColor,
+        ),
+        title: Text(
+          "More",
+          style: TextStyle(
+              color: currentIndex == 4
+                  ? DefaultColor.primaryColor
+                  : Colors.black),
+        ),
+      ),
+
+    ];
+    GlobalKey globalKey = new GlobalKey(debugLabel: 'btm_app_bar');
+    final BottomNavigationBar botNavBar = new BottomNavigationBar(
+      key: globalKey,
+      selectedFontSize: 12,
+      selectedItemColor: DefaultColor.buttonPrimary,
+//      fixedColor: DefaultColor.buttonPrimary,
+      unselectedLabelStyle: TextStyle(color: Colors.black),
+      selectedLabelStyle: TextStyle(color: DefaultColor.buttonPrimary),
+      items: _navigationViews
+          .map((NavigationIconView navigationView) => navigationView.item)
+          .toList(),
+      currentIndex: currentIndex,
+      type: BottomNavigationBarType.fixed,
+      onTap: (int index) {
+        setState(() {
+          this.currentIndex = index;
+        });
+      },
+    );
+
+    return new WillPopScope(
+        child: Scaffold(
+            body: buildWidgetUI(context), bottomNavigationBar: botNavBar),
+        onWillPop: () async {
+          bool keluar = false;
+          await showDialog(
+              context: context,
+              builder: (_) => AlertDialog(
+                title: Text("Anda akan keluar"),
+                content: Text("Anda yakin untuk keluar?"),
+                actions: <Widget>[
+                  new FlatButton(
+                    child: new Text("Ya",
+                        style: TextStyle(color: Colors.black38)),
+                    onPressed: () {
+                      pop();
+                      if (Platform.isAndroid)
+                        SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+                    },
+                  ),
+                  new FlatButton(
+                    child: new Text(
+                      "Tidak",
+                      style: TextStyle(color: DefaultColor.buttonPrimary),
+                    ),
+                    onPressed: () {
+                      pop();
+                      keluar = false;
+                    },
+                  ),
+                ],
+              ));
+          return keluar;
+        });
+  }
+}
+
+class NavigationIconView {
+  NavigationIconView({
+    Widget child,
+    Widget icon,
+    Widget activeIcon,
+    Widget title,
+    Color color,
+  })  : child = child,
+        item = new BottomNavigationBarItem(
+          icon: icon,
+          activeIcon: activeIcon,
+          title: title,
+          backgroundColor: color,
+        );
+
+  final Widget child;
+  final BottomNavigationBarItem item;
+}
+
+Widget buildIconAsset({
+  @required String asset,
+  double iconSize = 18,
+}) {
+  return Column(
+    children: <Widget>[
+      SvgPicture.asset(
+        asset,
+        color: Colors.grey,
+        height: iconSize,
+        width: iconSize,
+//        color: DefaultColor.buttonPrimary,
+      ),
+      Container(height: 6)
+    ],
+  );
+}
