7c99636654560c54ad32f41c0d6e586313988012
Averoes12 <hz.ir.daff@gmail.com>
2020-11-06 15:34:47 +0700

    modified facility model and add promo model

--- lib/model/facility-model.dart
+++ lib/model/facility-model.dart
@@ -1,16 +1,16 @@
 class FacilityModel {
   int id;
-  String facilityName;
+  String title;
   String tag;
   String image;
   String description;
 
   FacilityModel(
-      {this.id, this.facilityName, this.tag, this.image, this.description});
+      {this.id, this.title, this.tag, this.image, this.description});
 
   FacilityModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
-    facilityName = json['facilityName'];
+    title = json['title'];
     tag = json['tag'];
     image = json['image'];
     description = json['description'];
@@ -19,7 +19,7 @@ class FacilityModel {
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['id'] = this.id;
-    data['facilityName'] = this.facilityName;
+    data['title'] = this.title;
     data['tag'] = this.tag;
     data['image'] = this.image;
     data['description'] = this.description;
